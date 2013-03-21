194a7714a486abf66c70839a9ba6f8d79d239605
kylon <kylonhck@gmail.com>
2013-03-21 15:20:30 +0100

    removed backup files

--- end.sh
+++
@@ -1,5 +0,0 @@
-rm $HOME/sign/kernel/zImage
-rm $HOME/sign/system/lib/modules/*.ko
-cp arch/arm/boot/zImage ../../sign/kernel/zImage
-find . -name "*.ko" -exec cp {} ../../sign/system/lib/modules \;
-make clean -j10
