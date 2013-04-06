cc19a177831fb97a542ad446d59b6df4aa25c846
kylon <kylonhck@gmail.com>
2013-04-06 17:13:36 +0200

    Revert: Disable DHD P2P(from Andromadus). We need this otherwise the wifi will not be working

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 5 */
+/* This file is auto generated, version 8 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#5 PREEMPT Sat Apr 6 15:09:36 CEST 2013"
+#define UTS_VERSION "#8 PREEMPT Sat Apr 6 17:07:19 CEST 2013"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "kylon-linux"
 #define LINUX_COMPILER "gcc version 4.7.3 20130226 (prerelease) (Linaro GCC 4.7-2013.03) "
